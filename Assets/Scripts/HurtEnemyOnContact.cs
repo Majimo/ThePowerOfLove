39e3c3986419c6a5a68cb8cddf682bad826babcd
majimo <pierre.fervel@gmail.com>
2019-02-19 18:35:41 +0100

    Handle stomp on ennemies and add bounce effect

---
+++ Assets/Scripts/HurtEnemyOnContact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEnemyOnContact : MonoBehaviour
+{
+    public int damageOnContact;
+    public float bounceOnEnemy;
+    public Rigidbody2D parentRigidBody;
+
+    void Start()
+    {
+        parentRigidBody = transform.parent.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            collision.GetComponent<EnemyHealthManager>().takeDamage(damageOnContact);
+            parentRigidBody.velocity = new Vector2(parentRigidBody.velocity.x, bounceOnEnemy);
+        }
+    }
+}
