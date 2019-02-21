f5f24c13999fd5cb08cb1a24efc9b6739cffc5f0
majimo <pierre.fervel@gmail.com>
2019-02-21 22:54:28 +0100

    Add end to level & title screen backward

--- Assets/Scripts/DisplayHighScores.cs
+++ Assets/Scripts/DisplayHighScores.cs
@@ -8,31 +8,31 @@ public class DisplayHighScores : MonoBehaviour
 {
     private const int TOP_SCORE_COUNT = 3;
 
-    private List<PlayerScore> m_scores;
+    private List<PlayerScore> highScores;
 
-    [SerializeField] Text m_topScoreLabel01;
-    [SerializeField] Text m_topScoreLabel02;
-    [SerializeField] Text m_topScoreLabel03;
+    [SerializeField] Text topScoreLabel01;
+    [SerializeField] Text topScoreLabel02;
+    [SerializeField] Text topScoreLabel03;
 
     void Start()
     {
-        m_scores = new List<PlayerScore>();
-        m_scores.Add(new PlayerScore() { playerName = "Pierre", score = 5000 });
-        m_scores.Add(new PlayerScore() { playerName = "Pierre", score = 4000 });
-        m_scores.Add(new PlayerScore() { playerName = "Pierre", score = 3000 });
-        m_scores.Add(new PlayerScore() { playerName = "Pierre", score = 2000 });
-        m_scores.Add(new PlayerScore() { playerName = "Pierre", score = 1000 });
+        highScores = new List<PlayerScore>();
+        highScores.Add(new PlayerScore() { playerName = "Pierre", score = 5000 });
+        highScores.Add(new PlayerScore() { playerName = "Pierre", score = 4000 });
+        highScores.Add(new PlayerScore() { playerName = "Pierre", score = 3000 });
+        highScores.Add(new PlayerScore() { playerName = "Pierre", score = 2000 });
+        highScores.Add(new PlayerScore() { playerName = "Pierre", score = 1000 });
 
         PlayerScore[] top3Scores = GetHighScores(TOP_SCORE_COUNT);
 
         if (top3Scores.Length == TOP_SCORE_COUNT)
         {
-            if (m_topScoreLabel01)
-                m_topScoreLabel01.text = "1st place: " + top3Scores[0].playerName + ", " + top3Scores[0].score + " points";
-            if (m_topScoreLabel02)
-                m_topScoreLabel02.text = "2nd place: " + top3Scores[1].playerName + ", " + top3Scores[1].score + " points";
-            if (m_topScoreLabel03)
-                m_topScoreLabel03.text = "3rd place: " + top3Scores[2].playerName + ", " + top3Scores[2].score + " points";
+            if (topScoreLabel01)
+                topScoreLabel01.text = "1st place: " + top3Scores[0].playerName + ", " + top3Scores[0].score + " points";
+            if (topScoreLabel02)
+                topScoreLabel02.text = "2nd place: " + top3Scores[1].playerName + ", " + top3Scores[1].score + " points";
+            if (topScoreLabel03)
+                topScoreLabel03.text = "3rd place: " + top3Scores[2].playerName + ", " + top3Scores[2].score + " points";
         }
     }
 
@@ -44,7 +44,7 @@ public class DisplayHighScores : MonoBehaviour
 
     PlayerScore[] GetHighScores(int topCount)
     {
-        return m_scores.OrderByDescending(ps => ps.score).Take(topCount).ToArray();
+        return highScores.OrderByDescending(ps => ps.score).Take(topCount).ToArray();
     }
 
     public struct PlayerScore
