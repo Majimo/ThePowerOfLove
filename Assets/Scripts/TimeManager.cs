934e2d170484d7c93e48afe23b3b3a42a87cd849
majimo <pierre.fervel@gmail.com>
2019-02-26 19:13:24 +0100

    add timer to GUI

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public float timeLeft;
+    Text text;
+    
+    void Start()
+    {
+        timeLeft = 500.0f;
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = ((int) timeLeft) + "";
+    }
+}
