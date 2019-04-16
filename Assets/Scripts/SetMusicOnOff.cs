7f77500b715587c6837762b58843f3fcf401695d
majimo <pierre.fervel@gmail.com>
2019-04-16 19:11:19 +0200

    allow music btw scenes & avoid superposition on title screen

---
+++ Assets/Scripts/SetMusicOnOff.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMusicOnOff : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicPlayer>().StopMusic();
+        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicPlayer>().PlayMusic();
+    }
+}
